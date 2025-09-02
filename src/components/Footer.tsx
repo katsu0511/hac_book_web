06da32c8ce06fe768e874e0628980d6ae8700fc8
Katsuya <haradakatsuya190511@gmail.com>
2025-09-02 14:17:44 -0700

    created Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='bg-blue-500 w-full h-10'>
+      <div className='text-white text-xl max-w-screen-xl w-full h-full leading-10 text-center px-4 mx-auto'>&copy;Hac Book</div>
+    </footer>
+  );
+}
