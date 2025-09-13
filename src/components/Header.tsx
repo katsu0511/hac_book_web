2cc57f110651fef0327623dc14fe16b20263d160
Katsuya <haradakatsuya190511@gmail.com>
2025-09-13 10:31:44 -0700

    switch display by authentication

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,20 @@
+'use client';
+
+import Link from "next/link";
+import LogoutButton from './LogoutButton';
+import { useAuth } from '@/app/context/AuthContext';
+
 export default function Header() {
+  const { authenticated } = useAuth();
+
   return (
     <header className='bg-blue-500 w-full h-10'>
-      <div className='w-full h-full max-w-screen-xl px-4 mx-auto'>
-        <h1 className='text-white text-3xl font-bold leading-10'>Hac Book</h1>
+      <div className='flex justify-between max-w-screen-xl w-full h-full px-4 mx-auto'>
+        {authenticated
+          ? <Link href='/' className='text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>Hac Book</Link>
+          : <h1 className='text-white text-3xl font-bold leading-10'>Hac Book</h1>
+        }
+        {authenticated && <LogoutButton />}
       </div>
     </header>
   );
