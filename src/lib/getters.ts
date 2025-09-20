91b5f1fd90aebbcd9a81735619ec49a268ade474
Katsuya <haradakatsuya190511@gmail.com>
2025-09-20 16:17:20 -0700

    added a function to get parent categories

--- src/lib/getters.ts
+++ src/lib/getters.ts
@@ -19,3 +19,23 @@ export async function getMyCategories(): Promise<MyCategories | null | undefined
     return;
   }
 }
+
+export async function getParentCategories(): Promise<Category[] | null | undefined> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/parentCategory`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    });
+
+    if (!res.ok) return null;
+
+    const parentCategories: Category[] = await res.json();
+    return parentCategories;
+  } catch (error) {
+    console.log(error instanceof Error ? error.message : 'unknown error');
+    return;
+  }
+}
