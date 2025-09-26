f1a8705faf0c5ab60ea224179278f3259f65b641
Katsuya <haradakatsuya190511@gmail.com>
2025-09-26 14:37:45 -0700

    developed modifyCategory function

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,6 +1,6 @@
 'use client';
 
-export async function addCategories(data: CategoryFormData) {
+export async function addCategory(data: CategoryFormData) {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API}/add/category`, {
       method: 'POST',
@@ -12,6 +12,26 @@ export async function addCategories(data: CategoryFormData) {
     });
 
     if (!res.ok) return 'failed to add category';
+
+    const json = await res.json();
+    return json.category;
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
+
+export async function modifyCategory(data: CategoryFormData) {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/modify/category`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: data.id, parentId: data.parentId, name: data.name, type: data.type, description: data.description }),
+      credentials: 'include'
+    });
+
+    if (!res.ok) return 'failed to modify category';
 
     const json = await res.json();
     return json.category;
