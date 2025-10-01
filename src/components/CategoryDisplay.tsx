b092aec5770436dd7cd759d608dd7a652aa09ab5
Katsuya <haradakatsuya190511@gmail.com>
2025-10-01 16:02:15 -0700

    modified a bit
    changed conditional expression based on user id

--- src/components/CategoryDisplay.tsx
+++ src/components/CategoryDisplay.tsx
@@ -4,7 +4,7 @@ export default function CategoryDisplay({category}: {category: Category}) {
   return (
     <div>
       {
-        category.parentId === null
+        category.userId === null
         ? <p key={category.id} className='bg-green-200'>{category.name}</p>
         : <Link key={category.id} href={`/categories/modify/${category.id}`} className='bg-green-200'>{category.name}</Link>
       }
