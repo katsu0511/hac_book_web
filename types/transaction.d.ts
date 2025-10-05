b3a9c34371e9959c7a8708da399425b0f4a6706b
Katsuya <haradakatsuya190511@gmail.com>
2025-10-05 12:03:45 -0700

    Transaction type
    defined Transaction type

---
+++ types/transaction.d.ts
@@ -0,0 +1,11 @@
+type Transaction = {
+  id: string
+  userId: string
+  categoryId: string
+  amount: string
+  currency: string
+  description: string
+  transactionDate: string
+  createdAt: string
+  updatedAt: string
+};
