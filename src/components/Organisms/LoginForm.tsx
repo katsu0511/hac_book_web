8b6043fa2c523bc54c53d9e7e5e94ac05dc0600c
Katsuya <haradakatsuya190511@gmail.com>
2025-10-27 18:07:30 -0700

    separate functions
    separated functions around login and signup and introduced useAuthState

--- src/components/Organisms/LoginForm.tsx
+++ src/components/Organisms/LoginForm.tsx
@@ -1,51 +1,28 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import Input from './Input';
 import Button from '../Molecules/Button';
 import PageLink from '../Molecules/PageLink';
-import { useAuth } from '@/app/context/AuthContext';
-import { login } from '@/lib/actions';
+import useAuthState from '@/lib/hooks/useAuthState';
+import { handleLogin } from '@/lib/api/auth';
 
 export default function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const { authenticated, loading, refreshAuth } = useAuth();
-  const router = useRouter();
+  const { email, setEmail, password, setPassword, error, setError, authenticated, loading, refreshAuth, router } = useAuthState();
 
   useEffect(() => {
     if (loading) return;
     if (authenticated) router.replace('/');
   }, [authenticated, loading, router]);
 
-  const handleLogin = async (e: React.FormEvent) => {
+  const login = async (e: React.FormEvent) => {
     e.preventDefault();
-    setError('');
-    if (!email || !password) {
-      setError('input your email and password');
-      return;
-    }
-
-    const res = await login(email, password);
-    if (typeof res === 'string') {
-      setError(res);
-      return;
-    }
-    if (!res.ok) {
-      const json = await res.json();
-      if (json.loginFailed) setError(json.loginFailed);
-      return;
-    };
-
-    await refreshAuth();
-    router.replace('/');
+    handleLogin(email, password, setError, refreshAuth, router);
   };
 
   return (
     <div className='flex items-center w-full h-full'>
-      <form className='w-full' onSubmit={handleLogin}>
+      <form className='w-full' onSubmit={login}>
         <Input label='Email' type='text' value={email} autoComplete='email' onChange={(e) => setEmail(e.target.value)} />
         <Input label='Password' type='password' value={password} autoComplete='current-password' onChange={(e) => setPassword(e.target.value)} />
         <Button usage='Login' error={error} />
--- src/components/Organisms/SignupForm.tsx
+++ src/components/Organisms/SignupForm.tsx
@@ -1,60 +1,28 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import Input from './Input';
 import Button from '../Molecules/Button';
 import PageLink from '../Molecules/PageLink';
-import { useAuth } from '@/app/context/AuthContext';
-import { signup } from '@/lib/actions';
+import useAuthState from '@/lib/hooks/useAuthState';
+import { handleSignup } from '@/lib/api/auth';
 
 export default function SignupForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
-  const [error, setError] = useState('');
-  const { authenticated, loading, refreshAuth } = useAuth();
-  const router = useRouter();
+  const { name, setName, email, setEmail, password, setPassword, passwordConfirm, setPasswordConfirm, error, setError, authenticated, loading, refreshAuth, router } = useAuthState();
 
   useEffect(() => {
     if (loading) return;
     if (authenticated) router.replace('/');
   }, [authenticated, loading, router]);
 
-  const handleSignup = async(e: React.FormEvent<HTMLFormElement>) => {
+  const signup = async(e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError('');
-    if (!name || !email || !password || !passwordConfirm) {
-      setError('input your name, email and password');
-      return;
-    } else if (password !== passwordConfirm) {
-      setError('password doesn\'t match');
-      return;
-    }
-
-    const res = await signup(name, email, password);
-    if (typeof res === 'string') {
-      setError(res);
-      return;
-    }
-    if (!res.ok) {
-      const json = await res.json();
-      let errors = '';
-      if (json.email) errors += json.email;
-      if (json.password) errors += `\n${json.password}`;
-      if (json.signupFailed) errors += `\n${json.signupFailed}`;
-      setError(errors);
-      return;
-    };
-
-    await refreshAuth();
-    router.replace('/');
+    handleSignup(name, email, password, passwordConfirm, setError, refreshAuth, router);
   };
 
   return (
     <div className='flex items-center w-full h-full'>
-      <form className='w-full' onSubmit={handleSignup}>
+      <form className='w-full' onSubmit={signup}>
         <Input label='Name' type='text' value={name} autoComplete='name' onChange={(e) => setName(e.target.value)}/>
         <Input label='Email' type='text' value={email} autoComplete='email' onChange={(e) => setEmail(e.target.value)} />
         <Input label='Password' type='password' value={password} autoComplete='new-password' onChange={(e) => setPassword(e.target.value)}/>
