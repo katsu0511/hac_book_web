ff8210a80dcd1980cf621f1dc3ed1c3b59a53bbd
Katsuya <haradakatsuya190511@gmail.com>
2025-10-28 16:17:03 -0700

    categorize api lib
    moved actions.ts and getters.ts in subdirectory api in directory lib

---
+++ src/lib/api/actions.ts
@@ -0,0 +1,131 @@
+'use client';
+
+export async function login(email: string, password: string) {
+  try {
+    return await fetch(`${process.env.NEXT_PUBLIC_API}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+      credentials: 'include'
+    });
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
+
+export async function signup(name: string, email: string, password: string) {
+  try {
+    return await fetch(`${process.env.NEXT_PUBLIC_API}/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+      credentials: 'include'
+    });
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
+
+export async function addCategory(data: CategoryFormData) {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/categories`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ parentId: data.parentId, name: data.name, type: data.type, description: data.description }),
+      credentials: 'include'
+    });
+
+    if (!res.ok) return 'failed to add category';
+
+    const json = await res.json();
+    return json.category;
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
+
+export async function modifyCategory(data: CategoryFormData) {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/categories/${data.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: data.id, parentId: data.parentId, name: data.name, type: data.type, description: data.description }),
+      credentials: 'include'
+    });
+
+    if (!res.ok) return 'failed to modify category';
+
+    const json = await res.json();
+    return json.category;
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
+
+export async function addTransaction(data: TransactionFormData) {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/transactions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ categoryId: data.categoryId, amount: data.amount, currency: data.currency, description: data.description, transactionDate: data.transactionDate }),
+      credentials: 'include'
+    });
+
+    if (!res.ok) return 'failed to add transaction';
+
+    const transaction = await res.json();
+    return transaction;
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
+
+export async function modifyTransaction(data: TransactionFormData) {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/transactions/${data.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: data.id, categoryId: data.categoryId, amount: data.amount, currency: data.currency, description: data.description, transactionDate: data.transactionDate }),
+      credentials: 'include'
+    });
+
+    if (!res.ok) return 'failed to modify transaction';
+
+    const transaction = await res.json();
+    return transaction;
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
+
+export async function deleteTransaction(id: string) {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/transactions/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+      credentials: 'include'
+    });
+
+    if (!res.ok) return 'failed to delete transaction';
+
+    const deleted = await res.json();
+    return deleted;
+  } catch (error) {
+    return error instanceof Error ? error.message : 'unknown error';
+  }
+}
---
+++ src/lib/api/getters.ts
@@ -0,0 +1,101 @@
+'use client';
+
+export async function getCategory(id: string): Promise<Category | null | undefined> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/categories/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    });
+
+    if (!res.ok) return null;
+
+    const category: Category = await res.json();
+    return category;
+  } catch (error) {
+    console.log(error instanceof Error ? error.message : 'unknown error');
+    return;
+  }
+}
+
+export async function getMyCategories(): Promise<MyCategories | null | undefined> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/categories`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    });
+
+    if (!res.ok) return null;
+
+    const categories: MyCategories = await res.json();
+    return categories;
+  } catch (error) {
+    console.log(error instanceof Error ? error.message : 'unknown error');
+    return;
+  }
+}
+
+export async function getParentCategories(): Promise<Category[] | null | undefined> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/parent-categories`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    });
+
+    if (!res.ok) return null;
+
+    const parentCategories: Category[] = await res.json();
+    return parentCategories;
+  } catch (error) {
+    console.log(error instanceof Error ? error.message : 'unknown error');
+    return;
+  }
+}
+
+export async function getTransaction(id: string): Promise<Transaction | null | undefined> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/transactions/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    });
+
+    if (!res.ok) return null;
+
+    const transaction: Transaction = await res.json();
+    return transaction;
+  } catch (error) {
+    console.log(error instanceof Error ? error.message : 'unknown error');
+    return;
+  }
+}
+
+export async function getTransactions(): Promise<Transaction[] | null | undefined> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/transactions`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    });
+
+    if (!res.ok) return null;
+
+    const transactions: Transaction[] = await res.json();
+    return transactions;
+  } catch (error) {
+    console.log(error instanceof Error ? error.message : 'unknown error');
+    return;
+  }
+}
