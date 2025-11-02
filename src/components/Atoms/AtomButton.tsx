9e9a12c6a5cf79b06cc27818b228f188444b6913
Katsuya <haradakatsuya190511@gmail.com>
2025-11-02 10:35:03 -0800

    modified
    modified a bit

--- src/components/Atoms/AtomButton.tsx
+++ src/components/Atoms/AtomButton.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@mui/material';
 
-export default function AtomButton({ label, disabled }: { label: string, disabled: boolean }) {
+export default function AtomButton({ label, loading, disabled }: { label: string, loading: boolean, disabled: boolean }) {
   return (
     <Button type='submit' variant='outlined' color='primary' size='medium' disabled={disabled}>
-      {label}
+      {loading ? 'Loading…' : label}
     </Button>
   );
 }
