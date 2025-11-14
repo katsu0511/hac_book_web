db4ec72ad68c62234bb1773eda0e7314318b5788
Katsuya <haradakatsuya190511@gmail.com>
2025-11-14 14:16:18 -0800

    modified a bit
    changed variant outlined to contained

--- src/components/Atoms/AtomButton.tsx
+++ src/components/Atoms/AtomButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@mui/material';
 
 export default function AtomButton({ label, loading }: { label: string, loading: boolean }) {
   return (
-    <Button type='submit' variant='outlined' color='primary' size='medium' disabled={loading}>
+    <Button type='submit' variant='contained' color='primary' size='medium' disabled={loading}>
       {loading ? 'Loading…' : label}
     </Button>
   );
