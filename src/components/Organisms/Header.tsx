cb90bf0ba21a5654750796124aa21bb92e8347b5
Katsuya <haradakatsuya190511@gmail.com>
2025-11-16 11:30:37 -0800

    modified a bit
    modified css

--- src/components/Organisms/Header.tsx
+++ src/components/Organisms/Header.tsx
@@ -20,14 +20,14 @@ export default function Header() {
   }
 
   return (
-    <header className='bg-[color:var(--color-primary)] w-full h-10'>
+    <header className='bg-white w-full h-10 border-b border-[color:var(--color-primary)]'>
       <div className='flex justify-between w-full h-full md:max-w-screen-md xl:max-w-screen-xl px-4 mx-auto'>
         {authenticated
-          ? <Link href='/' className='flex text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60'>
+          ? <Link href='/' className='flex text-[color:var(--color-primary)] text-3xl font-bold leading-10 duration-300 hover:opacity-60'>
               <Image src="/books.png" alt="Books" width={40} height={40} />
               <p className={`${satisfy.className} ml-4`}>Hac Book</p>
             </Link>
-          : <h1 className='flex text-white text-3xl font-bold leading-10'>
+          : <h1 className='flex text-[color:var(--color-primary)] text-3xl font-bold leading-10'>
               <Image src="/books.png" alt="Books" width={40} height={40} />
               <p className={`${satisfy.className} ml-4`}>Hac Book</p>
             </h1>
