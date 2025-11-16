98def2140045212a7120d3ab0ca99368aaac8c4f
Katsuya <haradakatsuya190511@gmail.com>
2025-11-16 11:30:59 -0800

    modified a bit
    modified css

--- src/components/Atoms/LogoutButton.tsx
+++ src/components/Atoms/LogoutButton.tsx
@@ -1,5 +1,5 @@
 'use client';
 
 export default function LogoutButton(props: { logout: () => Promise<void> }) {
-  return <button className='bg-[color:var(--color-primary)] text-white h-10 w-20 duration-300 hover:bg-white hover:text-[color:var(--color-primary)] cursor-pointer' onClick={props.logout}>Logout</button>;
+  return <button className='bg-[color:var(--color-primary)] text-white h-10 w-20 border-b border-[color:var(--color-primary)] duration-300 hover:bg-white hover:text-[color:var(--color-primary)] cursor-pointer' onClick={props.logout}>Logout</button>;
 }
