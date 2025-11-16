befabfebeebf8a4e6170a425d4dec7369e21f527
Katsuya <haradakatsuya190511@gmail.com>
2025-11-16 11:31:15 -0800

    modified a bit
    modified css

--- src/components/Templates/Main.tsx
+++ src/components/Templates/Main.tsx
@@ -1,6 +1,6 @@
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main className='w-full h-[calc(100dvh-80px)]'>
+    <main className='w-full h-[calc(100dvh-80px)] bg-orange-50'>
       <div className='flex items-center w-full h-full md:max-w-screen-md xl:max-w-screen-xl px-4 mx-auto'>
         {children}
       </div>
