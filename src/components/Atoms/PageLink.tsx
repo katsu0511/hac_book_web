3ed52352dc9bed2d70a63dd3843db35e3e419621
Katsuya <haradakatsuya190511@gmail.com>
2025-11-15 17:06:27 -0800

    modified a bit
    modified css

--- src/components/Atoms/PageLink.tsx
+++ src/components/Atoms/PageLink.tsx
@@ -2,6 +2,6 @@ import Link from 'next/link';
 
 export default function PageLink(props: { page: string }) {
   return (
-    <Link href={`./${props.page}`} className='inline-block text-blue-500 font-bold duration-300 hover:opacity-40 cursor-pointer'>&gt;&gt; {`${props.page.charAt(0).toUpperCase()}${props.page.slice(1)}`}</Link>
+    <Link href={`./${props.page}`} className='inline-block text-[color:var(--color-primary)] font-bold duration-300 hover:opacity-40 cursor-pointer'>&gt;&gt; {`${props.page.charAt(0).toUpperCase()}${props.page.slice(1)}`}</Link>
   );
 }
