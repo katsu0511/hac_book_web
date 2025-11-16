3313d84aed9554f782239a0655a0621ed1eba0f5
Katsuya <haradakatsuya190511@gmail.com>
2025-11-16 11:31:08 -0800

    modified a bit
    modified css

--- src/components/Organisms/Footer.tsx
+++ src/components/Organisms/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className='bg-[color:var(--color-primary)] w-full h-10'>
-      <div className='text-white text-xl w-full h-full md:max-w-screen-md xl:max-w-screen-xl leading-10 text-center px-4 mx-auto'>&copy;Hac Book</div>
+    <footer className='bg-white w-full h-10 border-t border-[color:var(--color-primary)]'>
+      <div className='text-[color:var(--color-primary)] text-xl w-full h-full md:max-w-screen-md xl:max-w-screen-xl leading-10 text-center px-4 mx-auto'>&copy;Hac Book</div>
     </footer>
   );
 }
