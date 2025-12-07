aa31053b1776da91eb9566efc801687be5cc86c9
Katsuya <haradakatsuya190511@gmail.com>
2025-12-07 14:03:54 -0800

    use new API getSummary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,51 @@
+'use client';
+
+import { useState, useCallback, useEffect } from 'react';
+import { getSummary } from '@/lib/api/getters';
 import Link from 'next/link';
 
 export default function Home() {
+  const [income, setIncome] = useState<string>('');
+  const [expense, setExpense] = useState<string>('');
+  const [incomeBreakdown, setIncomeBreakdown] = useState<Record<string, string>>({});
+  const [expenseBreakdown, setExpenseBreakdown] = useState<Record<string, string>>({});
+
+  const fetchSummary = useCallback(async () => {
+    const summary = await getSummary();
+    if (summary === null) return;
+    else {
+      setIncome(summary.income);
+      setExpense(summary.expense);
+      setIncomeBreakdown(summary.incomeBreakdown);
+      setExpenseBreakdown(summary.expenseBreakdown);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchSummary();
+  }, [fetchSummary]);
+
   return (
     <div>
-      <h2>Hello World!</h2>
+      <h2>Income and Expense in this month</h2>
+      <p>Income: {income}</p>
+      <p>Expense: {expense}</p>
+      <div className='border-1 border-blue-500'>
+        {
+          Object.entries(expenseBreakdown).map(([key, value]) => (
+            <p key={key}>
+              {key}: {value}
+            </p>
+          ))
+        }
+        {
+          Object.entries(incomeBreakdown).map(([key, value]) => (
+            <p key={key}>
+              {key}: {value}
+            </p>
+          ))
+        }
+      </div>
       <div>
         <Link href={'/categories'}>Category</Link>
       </div>
