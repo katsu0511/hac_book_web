050716920950f35118d05ead71c7f3536b02e1f1
Katsuya <haradakatsuya190511@gmail.com>
2025-12-09 12:12:36 -0800

    new component
    created a new component LinkButton

---
+++ src/components/Atoms/LinkButton.tsx
@@ -0,0 +1,7 @@
+import Link from 'next/link';
+
+export default function LinkButton({ page, display}: { page: string, display: string }) {
+  return (
+    <Link href={`./${page}`} className='inline-block bg-[color:var(--color-primary)] text-white rounded-sm px-2 mx-2 duration-300 hover:opacity-40 cursor-pointer'>{display}</Link>
+  );
+}
