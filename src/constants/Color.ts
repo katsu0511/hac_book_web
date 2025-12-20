060774129d6ef5aae5c483a24c336a15fffca3ec
Katsuya <haradakatsuya190511@gmail.com>
2025-12-20 09:35:45 -0800

    define colore

---
+++ src/constants/Color.ts
@@ -0,0 +1,27 @@
+export enum Color {
+  lightBlue = '#4dc9f6',
+  orange = '#f67019',
+  pink = '#f53794',
+  blue = '#537bc4',
+  lightGreen = '#acc236',
+  navy = '#166a8f',
+  green = '#00a950',
+  gray = '#58595b',
+  purple = '#8549ba',
+  red = '#ce2727',
+  yellow = '#e8e552'
+}
+
+export const COLORS: readonly Color[] = [
+  Color.lightBlue,
+  Color.orange,
+  Color.pink,
+  Color.blue,
+  Color.lightGreen,
+  Color.navy,
+  Color.green,
+  Color.gray,
+  Color.purple,
+  Color.red,
+  Color.yellow,
+];
