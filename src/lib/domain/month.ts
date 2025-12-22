3dc591fb5a0d6a0897d2cc75f6849bb41fda8e87
Katsuya <haradakatsuya190511@gmail.com>
2025-12-22 11:28:48 -0800

    new function
    added getFirstDayOfCurrentMonth function

--- src/lib/domain/month.ts
+++ src/lib/domain/month.ts
@@ -1,6 +1,11 @@
 import { Month, MONTHS } from '@/constants/Month';
+import dayjs, { Dayjs } from 'dayjs';
 
 export const getCurrentMonth = (): Month => {
   const nowDate = new Date();
   return MONTHS[nowDate.getMonth()];
 };
+
+export const getFirstDayOfCurrentMonth = (): Dayjs => {
+  return dayjs().startOf('month');
+}
