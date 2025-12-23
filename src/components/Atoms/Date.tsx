239ceffddb4f1f6108a2af9031d5bf9bb46d4f99
Katsuya <haradakatsuya190511@gmail.com>
2025-12-23 11:59:15 -0800

    created date picker

---
+++ src/components/Atoms/Date.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Dayjs } from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+type Props = {
+  label: string
+  date: Dayjs
+  onChange: (newValue: Dayjs) => void
+  minDate?: Dayjs
+  maxDate?: Dayjs
+};
+
+export default function Date({label, date, onChange, minDate, maxDate}: Props) {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label={label}
+        value={date}
+        onChange={(newValue) => {
+          if (newValue) onChange(newValue);
+        }}
+        minDate={minDate}
+        maxDate={maxDate}
+        slotProps={{
+          textField: {
+            fullWidth: true,
+            sx: {
+              backgroundColor: 'white',
+              width: '300px',
+              margin: '0 10px',
+              '& fieldset': {
+                borderColor: 'primary.main',
+              },
+              '&:hover fieldset': {
+                borderColor: 'primary.main',
+                borderWidth: 2,
+                transition: 'all 0.1s',
+              },
+            },
+          }
+        }}
+      />
+    </LocalizationProvider>
+  );
+}
