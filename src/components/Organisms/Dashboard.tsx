ebe0cb8214c90635db201877ae6beab96b5d6e94
Katsuya <haradakatsuya190511@gmail.com>
2025-12-23 12:00:44 -0800

    updated Dashboard component

--- src/components/Organisms/Dashboard.tsx
+++ src/components/Organisms/Dashboard.tsx
@@ -1,27 +1,39 @@
 'use client';
 
+import { getCurrentMonth } from '@/lib/domain/month';
+import { Dayjs } from 'dayjs';
 import { useState, useCallback, useEffect } from 'react';
 import { getSummary } from '@/lib/api/getters';
 import Title from '@/components/Molecules/Title';
+import Date from '@/components/Atoms/Date';
 import DoughnutGraph from '@/components/Molecules/DoughnutGraph';
-import { getCurrentMonth } from '@/lib/domain/month';
 
 const month = getCurrentMonth();
 
-export default function Dashboard() {
+type Props = {
+  start?: Dayjs
+  end?: Dayjs
+  setStart?: (newValue: Dayjs) => void
+  setEnd?: (newValue: Dayjs) => void
+};
+
+export default function Dashboard({ start, end, setStart, setEnd }: Props) {
   const [income, setIncome] = useState<number>(0);
   const [expense, setExpense] = useState<number>(0);
   const [expenseBreakdown, setExpenseBreakdown] = useState<Record<string, number>>({});
 
+  const startStr = start ? start.format('YYYY-MM-DD') : undefined;
+  const endStr = end ? end.format('YYYY-MM-DD') : undefined;
+
   const fetchSummary = useCallback(async () => {
-    const summary = await getSummary();
+    const summary = await getSummary(startStr, endStr);
     if (summary === null) return;
     else {
       setIncome(summary.income);
       setExpense(summary.expense);
       setExpenseBreakdown(summary.expenseBreakdown);
     }
-  }, []);
+  }, [startStr, endStr]);
 
   useEffect(() => {
     fetchSummary();
@@ -29,7 +41,14 @@ export default function Dashboard() {
 
   return (
     <div className='w-full h-full'>
-      <Title />
+      <Title start={startStr} end={endStr} />
+      {
+        start && end && setStart && setEnd &&
+        <div className='w-160 mx-auto mb-10'>
+          <Date label='Start date' date={start} onChange={setStart} maxDate={end} />
+          <Date label='End date' date={end} onChange={setEnd} minDate={start} />
+        </div>
+      }
       <div className='flex justify-center items-center'>
         <div className='w-1/2 text-xl'>
           <div className='flex items-center py-3'>
