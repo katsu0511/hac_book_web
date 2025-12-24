ecfbdd820b79ccd6ad560b1acda44c79dbec32f8
Katsuya <haradakatsuya190511@gmail.com>
2025-12-24 14:41:09 -0800

    modified parameters

--- src/components/Atoms/PageLink.tsx
+++ src/components/Atoms/PageLink.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
-export default function PageLink(props: { page: string }) {
+export default function PageLink({page, display}: { page: string, display: string }) {
   return (
-    <Link href={`./${props.page}`} className='inline-block text-[color:var(--color-primary)] font-bold duration-300 hover:opacity-40 cursor-pointer'>&gt;&gt; {`${props.page.charAt(0).toUpperCase()}${props.page.slice(1)}`}</Link>
+    <Link href={`./${page}`} className='inline-block text-[color:var(--color-primary)] font-bold duration-300 hover:opacity-40 cursor-pointer'>&gt;&gt; {display}</Link>
   );
 }
