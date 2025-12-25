e41c425995af9edd615d61204958e7cbad01efe5
Katsuya <haradakatsuya190511@gmail.com>
2025-12-25 12:58:00 -0800

    modified a bit

--- src/components/Organisms/LoginForm.tsx
+++ src/components/Organisms/LoginForm.tsx
@@ -4,7 +4,7 @@ import useAuthState from '@/lib/hooks/useAuthState';
 import { useForm } from 'react-hook-form';
 import { handleLogin } from '@/lib/api/auth';
 import Form from '@/components/Organisms/Form';
-import FormElement from '../Molecules/FormElement';
+import AuthFormElement from '../Molecules/AuthFormElement';
 import SubmitButton from '../Molecules/SubmitButton';
 import LinkElement from '../Molecules/LinkElement';
 
@@ -25,8 +25,8 @@ export default function LoginForm() {
 
   return (
     <Form onSubmit={handleSubmit(login)}>
-      <FormElement name='email' label='Email' type='email' control={control} autoComplete='email' />
-      <FormElement name='password' label='Password' type='password' control={control} autoComplete='current-password' />
+      <AuthFormElement name='email' label='Email' type='email' control={control} autoComplete='email' />
+      <AuthFormElement name='password' label='Password' type='password' control={control} autoComplete='current-password' />
       <SubmitButton label='Login' error={error} loading={loadingState} />
       <LinkElement page='signup' display='Signup' />
     </Form>
