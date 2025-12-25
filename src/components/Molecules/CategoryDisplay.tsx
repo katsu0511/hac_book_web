811d0c343ef2008293b1f07be5eb4c7e8ceb9b2e
Katsuya <haradakatsuya190511@gmail.com>
2025-12-25 12:16:25 -0800

    move CategoryDisplay to Molecules

---
+++ src/components/Molecules/CategoryDisplay.tsx
@@ -0,0 +1,21 @@
+import { Category } from '@/types/category';
+import Link from 'next/link';
+
+export default function CategoryDisplay({category}: {category: Category}) {
+  const width = category.userId ? 'w-[calc(100%-40px)]' : 'w-full';
+
+  return (
+    <div key={category.id} className={`flex justify-between bg-[color:var(--color-primary)] ${width} text-white font-bold h-12 rounded-sm px-2 mb-1`}>
+      <Link href={`/categories/${category.id}`} className='text-xl leading-12 duration-300 hover:opacity-60'>{category.name}</Link>
+      {
+        category.userId &&
+        <Link
+          href={`/categories/modify/${category.id}`}
+          className='bg-white text-[color:var(--color-primary)] text-l h-8 leading-8 rounded-sm px-2 my-2 duration-300 hover:bg-[color:var(--color-primary)] hover:text-white'
+        >
+          Edit
+        </Link>
+      }
+    </div>
+  );
+}
