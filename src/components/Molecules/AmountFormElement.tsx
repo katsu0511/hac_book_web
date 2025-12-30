a910cb63a98d527bd745dbb985cc215382447606
Katsuya <haradakatsuya190511@gmail.com>
2025-12-30 12:43:56 -0800

    new component
    created AmountFormElement.tsx

---
+++ src/components/Molecules/AmountFormElement.tsx
@@ -0,0 +1,57 @@
+import { Control, Controller, FieldErrors } from 'react-hook-form';
+import { FormControl, TextField } from '@mui/material';
+
+type Props = {
+  errors: FieldErrors<TransactionFormData>
+  control: Control<TransactionFormData>
+};
+
+export default function AmountFormElement({ errors, control }: Props) {
+  return (
+    <FormControl fullWidth margin='normal'>
+      <Controller
+        name='amount'
+        control={control}
+        rules={{
+          required: 'amount is required',
+          pattern: {
+            value: /^\d+(\.\d{1,2})?$/,
+            message: 'Enter a valid amount with up to 2 decimal places'
+          }
+        }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label='Amount'
+            type='number'
+            autoComplete='off'
+            error={!!errors.amount}
+            helperText={errors.amount?.message}
+            variant='outlined'
+            slotProps={{
+              htmlInput: {
+                step: '0.01',
+                min: '0'
+              }
+            }}
+            sx={{
+              width: '300px',
+              margin: '0 auto',
+              '& .MuiOutlinedInput-root': {
+                backgroundColor: 'white',
+                '& fieldset': {
+                  borderColor: 'primary.main',
+                },
+                '&:hover fieldset': {
+                  borderColor: 'primary.main',
+                  borderWidth: 2,
+                  transition: 'all 0.1s',
+                },
+              },
+            }}
+          />
+        )}
+      />
+    </FormControl>
+  );
+}
