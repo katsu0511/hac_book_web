061e46a800d3b401b0ad0d99616bf896e55b8685
Katsuya <haradakatsuya190511@gmail.com>
2026-01-03 12:53:45 -0800

    demo login function
    added demo account info and login function with demo account

--- src/components/Organisms/LoginForm.tsx
+++ src/components/Organisms/LoginForm.tsx
@@ -6,6 +6,7 @@ import { handleLogin } from '@/lib/api/auth';
 import Form from '@/components/Organisms/Form';
 import AuthFormElement from '../Molecules/AuthFormElement';
 import SubmitButton from '../Molecules/SubmitButton';
+import { Box, Button } from '@mui/material';
 import LinkElement from '../Molecules/LinkElement';
 
 const defaultValues = {
@@ -15,7 +16,7 @@ const defaultValues = {
 
 export default function LoginForm() {
   const { loadingState, setLoadingState, error, setError, refreshAuth, router } = useAuthState();
-  const { control, handleSubmit } = useForm<AuthFormData>({ defaultValues });
+  const { control, handleSubmit, reset } = useForm<AuthFormData>({ defaultValues });
 
   const login = async (data: AuthFormData) => {
     setLoadingState(true);
@@ -23,11 +24,25 @@ export default function LoginForm() {
     setLoadingState(false);
   };
 
+  const inputDemoInfo = async () => {
+    const demoData: AuthFormData = {
+      name: '',
+      email: 'demo@example.com',
+      password: 'Demo@Example1',
+      passwordConfirm: ''
+    };
+    reset(demoData);
+    await login(demoData);
+  };
+
   return (
     <Form onSubmit={handleSubmit(login)}>
       <AuthFormElement name='email' label='Email' type='email' control={control} autoComplete='email' />
       <AuthFormElement name='password' label='Password' type='password' control={control} autoComplete='current-password' />
       <SubmitButton label='Login' error={error} loading={loadingState} />
+      <Box py={1}>
+        <Button variant='contained' color='warning' onClick={inputDemoInfo}>Demo account</Button>
+      </Box>
       <LinkElement page='signup' display='Signup' />
     </Form>
   );
