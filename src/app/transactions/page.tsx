6bb033e5640dd08701d7b5e77cdd93313cc34fad
Katsuya <haradakatsuya190511@gmail.com>
2026-01-04 12:06:53 -0800

    modified css
    modified css for responsive design

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -54,19 +54,19 @@ export default function Transactions() {
   return (
     <div className='pb-10'>
       <TitleLine title='Transactions' month={month} />
-      <div className='flex border-b-2 border-black-500 w-full h-10 leading-10 pl-3'>
-        <p className='w-40'>Category</p>
-        <p className='w-40'>Amount</p>
-        <p className='w-40'>Date</p>
+      <div className='flex border-b-2 border-black-500 w-full h-10 text-center leading-10 pl-0 md:text-left md:pl-3'>
+        <p className='w-1/3 md:w-40'>Category</p>
+        <p className='w-1/3 md:w-40'>Amount</p>
+        <p className='w-1/3 md:w-40'>Date</p>
       </div>
       {transactions.map(transaction => (
         <div key={transaction.id} className='flex justify-between h-15 border-b border-black-500'>
-          <Link href={`/transactions/${transaction.id}`} className='flex h-5 leading-5 pl-3 my-5 transition-all duration-300 hover:opacity-60'>
-            <p className='w-40'>{transaction.categoryName}</p>
-            <p className='w-40'>${transaction.amount}</p>
-            <p className='w-40'>{transaction.transactionDate}</p>
+          <Link href={`/transactions/${transaction.id}`} className='flex w-full h-5 leading-5 pl-0 my-5 transition-all duration-300 hover:opacity-60 md:pl-3'>
+            <p className='w-1/3 md:w-40'><span className='pl-[calc(50%-32px)] md:pl-0'>{transaction.categoryName}</span></p>
+            <p className='w-1/3 md:w-40'><span className='pl-[calc(50%-25px)] md:pl-0'>${transaction.amount}</span></p>
+            <p className='w-1/3 text-center md:w-40 md:text-left'>{transaction.transactionDate}</p>
           </Link>
-          <div className='flex gap-2 px-3'>
+          <div className='hidden gap-2 px-3 md:flex'>
             <Button variant='contained' onClick={() => onMoveToEdit(transaction.id)} sx={{ width: 80, height: 40, my: '10px' }}>Edit</Button>
             <Button variant='contained' color='error' onClick={() => onOpenDialog(transaction.id)} sx={{ width: 80, height: 40, my: '10px' }}>Delete</Button>
           </div>
