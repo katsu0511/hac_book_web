2e122688807bdc5b3ed6500cd8a898b7b7fb0993
Katsuya <haradakatsuya190511@gmail.com>
2026-01-04 11:57:45 -0800

    modified FormTitle
    deleted LinkButton and some parameters of FormTitle

--- src/components/Molecules/FormTitle.tsx
+++ src/components/Molecules/FormTitle.tsx
@@ -1,20 +1,9 @@
 'use client';
 
-import LinkButton from '@/components/Atoms/LinkButton';
-
-type Props = {
-  title: string
-  link: string
-  linkDisplay: string
-};
-
-export default function FormTitle({title, link, linkDisplay}: Props) {
+export default function FormTitle({title}: {title: string}) {
   return (
-    <h2 className='flex justify-between w-full border-b-2 border-gray-500 pt-5 mb-10'>
+    <h2 className='flex justify-between w-full border-b-2 border-gray-500 pt-5'>
       <p className='text-xl pl-3'>{title}</p>
-      <div>
-        <LinkButton page={`/${link}`} display={linkDisplay} />
-      </div>
     </h2>
   );
 }
