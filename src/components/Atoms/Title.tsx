37a847a6a92fdc36f65d8af10e6805e6043651ba
Katsuya <haradakatsuya190511@gmail.com>
2026-01-04 12:09:58 -0800

    modified UI
    modified UI for responsive design

--- src/components/Atoms/Title.tsx
+++ src/components/Atoms/Title.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 export default function Title({ title, month, start, end }: { title: string, month?: string, start?: string, end?: string }) {
-  const headingMonth = month ? <> in <span className='font-bold'>{month}</span></> : null;
-  const headingPeriod = start && end ? <> from <span className='font-bold'>{start}</span> to <span className='font-bold'>{end}</span></> : null;
+  const sp = <br className='block sm:hidden' />;
+  const headingMonth = month ? <>{sp} in <span className='font-bold'>{month}</span></> : null;
+  const headingPeriod = start && end ? <>{sp}<span className='font-bold'> {start}</span> ~ <span className='font-bold'>{end}</span></> : null;
   const heading = headingMonth ? headingMonth : headingPeriod ? headingPeriod : null;
   return <p className='text-2xl'>{title}{heading}</p>;
 }
