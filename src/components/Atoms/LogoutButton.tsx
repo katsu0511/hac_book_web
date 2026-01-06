b9e6df0eff0483230d1988077106c85d363ef423
Katsuya <haradakatsuya190511@gmail.com>
2026-01-06 10:59:44 -0800

    modified css and logic

--- src/components/Atoms/LogoutButton.tsx
+++ src/components/Atoms/LogoutButton.tsx
@@ -1,5 +1,14 @@
 'use client';
 
-export default function LogoutButton(props: { logout: () => Promise<void> }) {
-  return <button className='bg-[color:var(--color-primary)] text-white h-10 w-20 border-b border-[color:var(--color-primary)] duration-300 hover:bg-white hover:text-[color:var(--color-primary)] cursor-pointer' onClick={props.logout}>Logout</button>;
+import { handleLogout } from "@/lib/api/auth";
+import useAuthState from "@/lib/hooks/useAuthState";
+
+export default function LogoutButton() {
+  const { refreshAuth, router } = useAuthState();
+
+  const logout = async () => {
+    await handleLogout(refreshAuth, router);
+  }
+
+  return <button className='hidden bg-[color:var(--color-primary)] text-white h-10 w-20 border-b border-[color:var(--color-primary)] duration-300 cursor-pointer hover:bg-white hover:text-[color:var(--color-primary)] md:block' onClick={logout}>Logout</button>;
 }
