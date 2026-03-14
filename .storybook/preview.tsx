334f71c083f75b4027f6efbfdc798be7c2074071
Katsuya <haradakatsuya190511@gmail.com>
2026-03-14 13:38:31 -0700

    added preview.tsx

---
+++ .storybook/preview.tsx
@@ -0,0 +1,30 @@
+import '../src/app/globals.css';
+import type { Preview } from '@storybook/react';
+import ThemeProviderWrapper from '../src/components/Templates/ThemeProviderWrapper';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProviderWrapper>
+        <Story />
+      </ThemeProviderWrapper>
+    ),
+  ],
+};
+
+export default preview;
