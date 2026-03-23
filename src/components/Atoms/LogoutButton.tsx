64843fa13c061ad45d235d1a7eac05a5d05d4aab
Katsuya <haradakatsuya190511@gmail.com>
2026-03-22 18:20:10 -0700

    modified LogoutButton

--- src/components/Atoms/LogoutButton.tsx
+++ src/components/Atoms/LogoutButton.tsx
@@ -1,14 +1,27 @@
 'use client';
 
 import useAuthState from '@/lib/hooks/useAuthState';
-import { handleLogout } from '@/lib/api/auth';
+import { handleLogout as defaultHandleLogout } from '@/lib/api/auth';
 
-export default function LogoutButton() {
+export default function LogoutButton({
+  className = '',
+  onLogout = defaultHandleLogout
+}: {
+  className?: string;
+  onLogout?: typeof defaultHandleLogout
+}) {
   const { refreshAuth, router } = useAuthState();
 
   const logout = async () => {
-    await handleLogout(refreshAuth, router);
+    await onLogout(refreshAuth, router);
   }
 
-  return <button className='hidden bg-[color:var(--color-primary)] text-white h-10 w-20 border-b border-[color:var(--color-primary)] duration-300 cursor-pointer hover:bg-white hover:text-[color:var(--color-primary)] md:block' onClick={logout}>Logout</button>;
+  return (
+    <button
+      className={`hidden bg-[color:var(--color-primary)] text-white h-10 w-20 border-b border-[color:var(--color-primary)] duration-300 cursor-pointer hover:bg-white hover:text-[color:var(--color-primary)] md:block ${className}`}
+      onClick={logout}
+    >
+      Logout
+    </button>
+  );
 }
