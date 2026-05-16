b115b60c83cee730a444b3840dcb07094be9f387
Katsuya <haradakatsuya190511@gmail.com>
2026-05-16 16:07:24 -0700

    added setOpenMenu to manage menu in header

--- src/components/Atoms/LogoutButton.tsx
+++ src/components/Atoms/LogoutButton.tsx
@@ -3,25 +3,13 @@
 import useAuthState from '@/lib/hooks/useAuthState';
 import { handleLogout as defaultHandleLogout } from '@/lib/api/auth';
 
-export default function LogoutButton({
-  className = '',
-  onLogout = defaultHandleLogout
-}: {
-  className?: string;
-  onLogout?: typeof defaultHandleLogout
-}) {
+export default function LogoutButton({ onLogout = defaultHandleLogout, setOpenMenu }: { onLogout?: typeof defaultHandleLogout, setOpenMenu: (openMenu: boolean) => void }) {
   const { refreshAuth, router } = useAuthState();
 
   const logout = async () => {
+    setOpenMenu(false);
     await onLogout(refreshAuth, router);
   }
 
-  return (
-    <button
-      className={`hidden bg-[color:var(--color-primary)] text-white h-10 w-20 border-b border-[color:var(--color-primary)] duration-300 cursor-pointer hover:bg-white hover:text-[color:var(--color-primary)] md:block ${className}`}
-      onClick={logout}
-    >
-      Logout
-    </button>
-  );
+  return <button className={`block bg-[color:var(--color-primary)] text-white w-44 rounded-sm mx-3 my-2 duration-300 cursor-pointer hover:opacity-60`} onClick={logout}>Logout</button>;
 }
