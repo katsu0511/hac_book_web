6a7a1895b81de940affcdda2e748bd6707ddacea
Katsuya <haradakatsuya190511@gmail.com>
2026-06-01 20:47:35 -0700

    new page
    created setting page

---
+++ src/app/setting/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import useAuthState from '@/lib/hooks/useAuthState';
+import { AuthRedirectToLogin } from '@/lib/auth/AuthRedirect';
+import FormTitle from '@/components/Molecules/FormTitle';
+import Row from '@/components/Atoms/Row';
+import LinkElement from '@/components/Molecules/LinkElement';
+
+export default function Profile() {
+  const { profile } = useAuthState();
+
+  return (
+    <AuthRedirectToLogin>
+      <FormTitle title='Setting' />
+      <div className='pt-10'>
+        <div className='w-full h-full md:max-w-screen-md mx-auto mt-10'>
+          <Row head='Currency' body={profile?.setting.currency ?? ''} />
+          <Row head='Language' body={profile?.setting.language ?? ''} />
+          <Row head='Monthly Saving Goal' body={`$${profile?.setting.monthlySavingGoal ?? ''}`} />
+        </div>
+        <LinkElement page='saving-goal' display='Change Monthly Saving Goal' />
+      </div>
+    </AuthRedirectToLogin>
+  );
+}
