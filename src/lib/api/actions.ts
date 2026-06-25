3dbc3157d4a759547d0fef1978ab9bd52e8dfb16
Katsuya <haradakatsuya190511@gmail.com>
2026-06-25 11:10:43 -0700

    added a new function changeUserPassword

--- src/lib/api/actions.ts
+++ src/lib/api/actions.ts
@@ -228,3 +228,26 @@ export async function changeUserEmail(data: User): Promise<Result> {
     return { ok: false, error: message };
   }
 }
+
+export async function changeUserPassword(data: PasswordFormData): Promise<Result> {
+  try {
+    const res = await fetch(`/api/profile/password`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ oldPassword: data.currentPassword, newPassword: data.newPassword }),
+      credentials: 'include'
+    });
+
+    if (!res.ok) {
+      const data = await res.json();
+      return { ok: res.ok, error: data.message };
+    }
+
+    return { ok: res.ok, response: res };
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'Unknown error occurred.';
+    return { ok: false, error: message };
+  }
+}
