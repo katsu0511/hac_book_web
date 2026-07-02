d77fae411cc3145d041e5c2e196d80ee69f50194
Katsuya <haradakatsuya190511@gmail.com>
2026-07-01 17:41:27 -0700

    modified Toast

--- src/components/Molecules/ToastForChangeInfo.tsx
+++ src/components/Molecules/ToastForChangeInfo.tsx
@@ -4,6 +4,7 @@ import { Dispatch, SetStateAction } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Snackbar, Alert } from '@mui/material';
 
 type Props = {
+  infoType: string
   dialogOpen: boolean
   snackbarOpen: boolean
   buttonLoading: boolean
@@ -12,19 +13,19 @@ type Props = {
   changeName: () => Promise<void>
 };
 
-export default function ToastForChangeInfo({dialogOpen, snackbarOpen, buttonLoading, setSnackbarOpen, handleCancel, changeName}: Props) {
+export default function ToastForChangeInfo({ infoType, dialogOpen, snackbarOpen, buttonLoading, setSnackbarOpen, handleCancel, changeName }: Props) {
   return (
     <>
       <Dialog open={dialogOpen} onClose={handleCancel}>
-        <DialogTitle>Email Change Confirmation</DialogTitle>
-        <DialogContent>Are you sure to change your email? <strong className='text-red-500'>After this process, you will logged out. Please login again.</strong></DialogContent>
+        <DialogTitle>Change Confirmation</DialogTitle>
+        <DialogContent>Are you sure to change your {infoType}? <strong className='text-red-500'>After this process, you will logged out. Please login again.</strong></DialogContent>
         <DialogActions>
           <Button onClick={handleCancel} color='error' disabled={buttonLoading}>Cancel</Button>
           <Button onClick={changeName} color='primary' autoFocus disabled={buttonLoading}>OK</Button>
         </DialogActions>
       </Dialog>
       <Snackbar open={snackbarOpen} autoHideDuration={2000} onClose={() => setSnackbarOpen(false)}>
-        <Alert severity='success' variant='filled'>Successfully changed your email</Alert>
+        <Alert severity='success' variant='filled'>Successfully changed your {infoType}</Alert>
       </Snackbar>
     </>
   );
